b513ca4988345ac4fa5c5e52f0626ab0591ef08d
ak95152 <124942925+coderAK3@users.noreply.github.com>
2023-02-10 00:42:07 +0000

    made matplotlib line graph

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-import pandas as pd 
+
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import numpy as np
-import datetime as DT
 
+linear_data = np.array([1, 20, 40, 60, 80, 100])
 
-df = pd.read_csv('https://raw.githubusercontent.com/cwkteacher/Data/master/athletes.csv')
+fig = plt.figure()
+plt.plot(linear_data, '-x')
 
-#print(df['sport'])
-#print(df[['sport', 'name']])
-
-# athletic = df.where(df['sport'] == 'athletics')
-# print(athletic.head())
-
-print(df['nationality'].value_counts()[40:50])
-
-gold = df.groupby('nationality').sum()['gold']
-silver = df.groupby('nationality').sum()['silver']
-bronze = df.groupby('nationality').sum()['bronze']
-
-total_medals = gold + silver + bronze
-
-now = pd.Timestamp(DT.datetime(2016, 10, 5, 18, 00))
-print(now)
-
-df['dob'] = pd.to_datetime(df['dob'])
-df['dob'] = df['dob'].where(df['dob'] < now, df['dob'] - np.timedelta64(100, 'Y'))
-df['age'] = (now - df['dob']).astype('<m8[Y]')
-print(df)
+fig.savefig('line.png')
